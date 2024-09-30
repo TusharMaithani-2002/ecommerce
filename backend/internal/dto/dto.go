940d6fa5e6330a2002eb4fcd84e1961eabafb5a3
Tushar Maithani <202051194@iiitvadodara.ac.in>
2024-09-30 22:49:45 +0530

    added response field names according rest api convention

--- backend/internal/dto/dto.go
+++ backend/internal/dto/dto.go
@@ -7,60 +7,60 @@ import (
 
 // while sending user at
 type UserResponse struct {
-	ID          int
-	Email       string
-	Address     string
-	Name        string
-	Role        string
-	PhoneNumber string
+	ID          int    `json:"id"`
+	Email       string `json:"email"`
+	Address     string `json:"address"`
+	Name        string `json:"name"`
+	Role        string `json:"role"`
+	PhoneNumber string `json:"phoneNumber"`
 }
 
 type ProductReponse struct {
-	ID          int
-	Name        string
-	Price       float64
-	Category    string
-	Images      datatypes.JSON
-	Quantity    int
-	SellerId    int
-	Description string
-	Seller      UserResponse
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+	ID          int            `json:"id"`
+	Name        string         `json:"name"`
+	Price       float64        `json:"price"`
+	Category    string         `json:"category"`
+	Images      datatypes.JSON `json:"images"`
+	Quantity    int            `json:"quantity"`
+	SellerId    int            `json:"sellerId"`
+	Description string         `json:"description"`
+	Seller      UserResponse   `json:"seller"`
+	CreatedAt   time.Time      `json:"createdAt"`
+	UpdatedAt   time.Time      `json:"updatedAt"`
 }
 
 type ProductNoSellerResponse struct {
-	ID          int
-	Name        string
-	Price       float64
-	Category    string
-	Images      datatypes.JSON
-	Quantity    int
-	SellerId    int
-	Description string
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+	ID          int            `json:"id"`
+	Name        string         `json:"name"`
+	Price       float64        `json:"price"`
+	Category    string         `json:"category"`
+	Images      datatypes.JSON `json:"images"`
+	Quantity    int            `json:"quantity"`
+	SellerId    int            `json:"sellerId"`
+	Description string         `json:"description"`
+	CreatedAt   time.Time      `json:"createdAt"`
+	UpdatedAt   time.Time      `json:"updatedAt"`
 }
 
 type UpdatedProduct struct {
-	Name        *string
-	Price       *float64
-	Description *string
-	Images      *datatypes.JSON
-	Quantity    *int
-	Category    *string
+	Name        *string         `json:"name"`
+	Price       *float64        `json:"price"`
+	Description *string         `json:"description"`
+	Images      *datatypes.JSON `json:"images"`
+	Quantity    *int            `json:"quantity"`
+	Category    *string         `json:"category"`
 }
 
 type UpdatedUser struct {
-	Name        *string
-	Address     *string
-	Role        *string
-	PhoneNumber *string
+	Name        *string `json:"name"`
+	Address     *string `json:"address"`
+	Role        *string `json:"role"`
+	PhoneNumber *string `json:"phoneNumber"`
 }
 
 type PaginatedProductsResponse struct {
-	Products    []ProductNoSellerResponse
-	CurrentPage int
-	PageSize    int
-	NextPage    bool
+	Products    []ProductNoSellerResponse `json:"products"`
+	CurrentPage int                       `json:"currentPage"`
+	PageSize    int                       `json:"pageSize"`
+	NextPage    bool                      `json:"nextPage"`
 }
